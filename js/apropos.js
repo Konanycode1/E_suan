a9ed7e4169eaaac648f7c29c9352a5a187e85066
kskone <kskone07@gmail.com>
2023-04-25 17:06:29 +0000

    eqsrdtfy

---
+++ js/apropos.js
@@ -0,0 +1,11 @@
+// const slide = ["/image/image enfants.jpeg","/image/masque senefou.jpeg"];
+// let numero = 0;
+
+// function ChangeSlide(sens) {
+//     numero = numero + sens;
+//     if (numero < 0)
+//         numero = slide.length - 1;
+//     if (numero > slide.length - 1)
+//         numero = 0;
+//     document.getElementById("slide").src = slide[numero];
+// }
