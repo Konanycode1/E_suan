794422cb0f5a8d87b2e4685c106fa41e1bb3547d
kouame konan abraham <abrahamkonany@gmail.com>
2023-04-28 10:50:20 +0000

    ok

--- js/authentificate.js
+++ js/authentificate.js
@@ -1,3 +1,70 @@
 $(document).ready(()=>{
-    
+    let mydata ={
+        id : 1,
+        nom: "ab",
+        prenom:"ko",
+        niveau: "ce2",
+        numero: "14566",
+        pwd: "12345"
+      }
+
+      let btnBaoule = $("#btnBaoule");
+      let btnBete = $("#btnBete");
+      let btnSenoufo = $("#btnSenoufo");
+      let btnYacouba = $("#btnYacouba");
+      let modif = $(".modif");
+
+      let btnGlobal = $(document.querySelectorAll(" .langu "));
+      console.log(btnGlobal)
+
+      let student = new Student(mydata);
+      let session = localStorage.getItem("session");
+      session = JSON.parse(session);
+      console.log(session);
+
+      if(session == null ){
+        btnGlobal.on("click", (e)=>{
+            window.location.href = "../pages/login.html";
+        });
+      }
+      else{
+        deconnexion();
+        actionLang();
+      }
+
+      function deconnexion() {
+        modif.text("deconnexion");
+        modif.on("click", ()=>{
+            localStorage.removeItem("session")
+        })
+      }
+
+      function actionLang() {
+        btnGlobal.on("click",(e)=>{
+            let link = e.target.id;
+
+            switch (link) {
+                case "btnBaoule":
+                  window.location.href = "../pages/langueBaoule.html";
+                  break;
+                case "btnBete":
+                  window.location.href = "../pages/langueBete.html";
+                  break;
+                case "btnSenoufo":
+                  window.location.href = "../pages/langueSenoufo.html";
+                  break;
+                case "btnYacouba":
+                  window.location.href = "../pages/langueSenoufo.html";
+                  break;
+                default:
+                    break;
+            }
+        })
+        
+        
+      }
+      
+
+
+
 })
--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
-let btn = $(document.getElementById("btnBaoule"));
+// let btn = $(document.getElementById("btnBaoule"));
 
-btn.click(function (e) {
-    // $("#btnBaoulé").prop("location","./pages/langueBaoule.html")
-    console.log(e)
+// btn.click(function (e) {
+//     $("#btnBaoulé").prop("location","./pages/langueBaoule.html")
+//     console.log(e)
     
+// })
+let mydata ={
+    id : 1,
+    nom: "ab",
+    prenom:"ko",
+    niveau: "ce2",
+    numero: "14566",
+    pwd: "12345"
+  }
+  let student = new Student(mydata)
+
+$(document).ready(()=>{
+   let logo  = $("#esuan");
+
+    logo.on("click",()=>{
+        window.location.href = "../index.html";
+    })
+
+   let ajout = $(".userName");
+    let session = localStorage.getItem("session");
+    session = JSON.parse(session);
+    console.log(session);
+   let recup = student.getAdd();
+   recup.forEach(element => {
+    console.log(element.numero)
+    if(element.numero === session.user){
+        ajout.text(`Mr ${element.nom} ${element.nom}`)
+    }
+   });
+
 })
